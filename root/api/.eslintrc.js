1b5d189930ebb7cb0da903d7d467761d57118010
freshc0w <121275444+freshc0w@users.noreply.github.com>
2023-08-07 19:13:42 +1000

    config eslint@

---
+++ root/api/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+	env: {
+		browser: true,
+		commonjs: true,
+		es2021: true,
+	},
+	extends: 'standard',
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: ['.eslintrc.{js,cjs}'],
+			parserOptions: {
+				sourceType: 'script',
+			},
+		},
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+	},
+	rules: {
+		indent: [0, 4],
+		quotes: ['error', 'single'],
+		eqeqeq: 'error',
+		'no-trailing-spaces': 'error',
+		'object-curly-spacing': ['error', 'always'],
+		'arrow-spacing': ['error', { before: true, after: true }],
+		'no-console': 0,
+		'no-unused-vars': 'warn',
+		'no-tabs': 0,
+		'comma-dangle': 0,
+		semi: 0,
+	},
+};
