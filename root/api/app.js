14f2caf3f4eaf9f7cd8b37a29fa1d4de5584d85a
freshc0w <121275444+freshc0w@users.noreply.github.com>
2023-08-08 15:56:01 +1000

    Config MONGODB_URI to NODE_ENV

--- root/api/app.js
+++ root/api/app.js
@@ -1,54 +1,54 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 // configurations
-const config = require('./utils/config')
-const middleware = require('./utils/middleware')
-const morgan = require('morgan')
-const logger = require('./utils/logger')
-const mongoose = require('mongoose')
+const config = require('./utils/config');
+const middleware = require('./utils/middleware');
+const morgan = require('morgan');
+const logger = require('./utils/logger');
+const mongoose = require('mongoose');
 
-require('express-async-errors')
+require('express-async-errors');
 
 // cors
-const cors = require('cors')
+const cors = require('cors');
 
 // routers
-const pingRouter = require('./controllers/ping')
+const pingRouter = require('./controllers/ping');
 
-app.use(cors())
-app.use(express.static('dist'))
-app.use(express.json())
+app.use(cors());
+app.use(express.static('dist'));
+app.use(express.json());
 
 // connecting to MONGODB
-mongoose.set('strictQuery', false)
-const url = config.MONGODB_URI
-logger.info('connecting to', url)
+mongoose.set('strictQuery', false);
+const url = config.MONGODB_URI;
+logger.info('connecting to', url);
 mongoose
-  .connect(url)
-  .then(res => {
-    logger.info('connected to MongoDB')
-  })
-  .catch(err => {
-    logger.info(`error connecting to MongoDB: ${err.message}`)
-  })
+	.connect(url)
+	.then(res => {
+		logger.info('connected to MongoDB');
+	})
+	.catch(err => {
+		logger.info(`error connecting to MongoDB: ${err.message}`);
+	});
 
 // middleware
 app
-  .use(middleware.requestLogger)
-  .use(morgan(middleware.reqMorganLogger))
-  .use(middleware.userExtractor)
+	.use(middleware.requestLogger)
+	.use(morgan(middleware.reqMorganLogger))
+	.use(middleware.userExtractor);
 
 // controllers
-app.use('/api/ping', pingRouter)
+app.use('/api/ping', pingRouter);
 
 if (process.env.NODE_ENV === 'test') {
-  const testingRouter = require('./controllers/testing')
-  app.use('/api/testing', testingRouter)
+	const testingRouter = require('./controllers/testing');
+	app.use('/api/testing', testingRouter);
 }
 
 // error handling
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
-module.exports = app
+module.exports = app;
--- root/api/utils/config.js
+++ root/api/utils/config.js
@@ -2,7 +2,18 @@
 require('dotenv').config();
 
 const PORT = process.env.PORT;
-const MONGODB_URI = process.env.MONGODB_URI;
-const TEST_MONGODB_URI = process.env.TEST_MONGODB_URI;
 
-module.exports = { PORT, MONGODB_URI, TEST_MONGODB_URI };
+const getMongoUri = () => {
+	switch (process.env.NODE_ENV) {
+		case 'test':
+			return process.env.TEST_MONGODB_URI;
+		case 'development':
+			return process.env.DEV_MONGODB_URI;
+		default:
+			return process.env.MONGODB_URI;
+	}
+};
+
+const MONGODB_URI = getMongoUri();
+
+module.exports = { PORT, MONGODB_URI };
