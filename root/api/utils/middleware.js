d14e110a7946cd22547b4f586d571eb12202de45
freshc0w <121275444+freshc0w@users.noreply.github.com>
2023-08-08 20:14:07 +1000

    Fix middleware errorHandler bug

--- root/api/utils/middleware.js
+++ root/api/utils/middleware.js
@@ -59,16 +59,16 @@ const errorHandler = (error, req, res, next) => {
 	logger.error(error.message);
 
 	if (error.name === 'CastError') {
-		return response.status(400).send({ error: 'malformatted id' });
+		return res.status(400).send({ error: 'malformatted id' });
 	} else if (error.name === 'ValidationError') {
 		// Invalid params from mongoose validation schema
-		return response.status(400).json({ error: error.message });
+		return res.status(400).json({ error: error.message });
 	} else if (error.name === 'ReferenceError') {
-		return response.status(400).json({ error: error.message });
+		return res.status(400).json({ error: error.message });
 	} else if (error.name === 'JsonWebTokenError') {
-		return response.status(400).json({ error: error.message });
+		return res.status(400).json({ error: error.message });
 	} else if (error.name === 'TokenExpiredError') {
-		return response.status(401).json({ error: 'token expired' });
+		return res.status(401).json({ error: 'token expired' });
 	}
 
 	next(error);
