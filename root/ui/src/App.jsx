7906c1897dacd2397d1adfc9307800e03e703744
freshc0w <121275444+freshc0w@users.noreply.github.com>
2023-08-12 17:58:31 +1000

    Add LoginForm component

--- root/ui/src/App.jsx
+++ root/ui/src/App.jsx
@@ -10,31 +10,37 @@ import userServices from './services/users';
 import courseServices from './services/courses';
 import professorServices from './services/professors';
 
+// Components
+import LoginForm from './components/LoginForm';
+
 const getUserData = async () => {
-  const response = await userServices.getAll();
-  console.log(response);
-}
+	const response = await userServices.getAll();
+	console.log(response);
+};
 
 const getCourseData = async () => {
-  const response = await courseServices.getAll();
-  console.log(response);
-}
+	const response = await courseServices.getAll();
+	console.log(response);
+};
 
 const getProfessorData = async () => {
-  const response = await professorServices.getAll();
-  console.log(response);
-}
+	const response = await professorServices.getAll();
+	console.log(response);
+};
 
 const App = () => {
+	useEffect(() => {
+		console.log('App initialised');
+		getUserData();
+		getCourseData();
+		getProfessorData();
+	}, []);
 
-  useEffect(() => {
-    console.log('App initialised');
-    getUserData();
-    getCourseData();
-    getProfessorData();
-  }, [])
-
-  return <>App initialised</>
-}
+	return (
+		<>
+			<LoginForm />
+		</>
+	);
+};
 
 export default App;
---
+++ root/ui/src/components/LoginForm.jsx
@@ -0,0 +1,71 @@
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { setUser } from '../reducers/userReducer';
+
+import loginService from '../services/login';
+
+const LoginForm = () => {
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		const loggedUserJSON = window.localStorage.getItem('loggedUser');
+		if (loggedUserJSON) {
+			const user = JSON.parse(loggedUserJSON);
+			dispatch(setUser(user));
+
+			// TODO: set token for course reviews and professor reviews
+		}
+	}, [dispatch]);
+
+	const handleLogin = async e => {
+		e.preventDefault();
+
+		try {
+			const user = await loginService.login({
+				username,
+				password,
+			});
+
+			window.localStorage.setItem('loggedUser', JSON.stringify(user));
+
+			// TODO: set token for course reviews and professor reviews
+
+			dispatch(setUser(user));
+
+			setUsername('');
+			setPassword('');
+		} catch (err) {
+			console.error(err);
+		}
+	};
+
+	return (
+		<form onSubmit={handleLogin}>
+			<div>
+				username
+				<input
+					type="text"
+					value={username}
+					name="Username"
+					onChange={({ target }) => setUsername(target.value)}
+				/>
+			</div>
+			<div>
+				password
+				<input
+					type="password"
+					value={password}
+					name="Password"
+					onChange={({ target }) => setPassword(target.value)}
+				/>
+			</div>
+			<button type="submit">login</button>
+		</form>
+	);
+};
+
+export default LoginForm;
