8ea5cd20492183a07ab173d2a5ee1d0d2f0e9c69
freshc0w <121275444+freshc0w@users.noreply.github.com>
2023-08-14 17:26:18 +1000

    Add courses and professors reducer to store

--- root/ui/src/store.js
+++ root/ui/src/store.js
@@ -2,6 +2,8 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import userReducer from './reducers/userReducer';
 import usersReducer from './reducers/usersReducer';
+import coursesReducer from './reducers/coursesReducer';
+import professorsReducer from './reducers/professorsReducer';
 import courseReviewsReducer from './reducers/courseReviewsReducer';
 import professorReviewsReducer from './reducers/professorReviewsReducer';
 
@@ -9,6 +11,8 @@ const store = configureStore({
   reducer: {
     user: userReducer,
     users: usersReducer,
+    courses: coursesReducer,
+    professors: professorsReducer,
     courseReviews: courseReviewsReducer,
     professorReviews: professorReviewsReducer,
   }
