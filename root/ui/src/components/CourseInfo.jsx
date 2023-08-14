28b0880f58de6d72a342222275f76a7d32df0b5e
freshc0w <121275444+freshc0w@users.noreply.github.com>
2023-08-14 19:40:13 +1000

    Refactor course and professor info list

--- root/ui/src/components/CourseInfo.jsx
+++ root/ui/src/components/CourseInfo.jsx
@@ -1,27 +1,34 @@
-import { useNavigate } from "react-router-dom";
-import Td from "./Td"; // helper that wraps content into a Link if a link is provided
+import { useNavigate } from 'react-router-dom';
+import Td from './Td'; // helper that wraps content into a Link if a link is provided
 
 const CourseInfo = ({ course }) => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 	const { code, title, avgRating, faculty, professor, reviews } = course;
 
-  const clickableStyle = {
-    cursor: 'pointer',
-  }
+	const clickableStyle = {
+		cursor: 'pointer',
+	};
 
-  // onClick goes to a page with the course's reviews
-  const handleClick = () => {
-    navigate(`/courses/${course.id}`);
-  }
+	// onClick goes to a page with the course's reviews
+	const handleClick = () => {
+		navigate(`/courses/${course.id}`);
+	};
 
 	return (
-		<tr style={clickableStyle} onClick={handleClick}>
-			<Td to={`/courses/${course.id}`}>{code}</Td>
-			<Td to={`/courses/${course.id}`}>{title}</Td>
-			<Td to={`/courses/${course.id}`}>{avgRating}</Td>
-			<Td to={`/courses/${course.id}`}>{faculty}</Td>
-			<Td to={`/courses/${course.id}`}>{professor}</Td>
-			<Td to={`/courses/${course.id}`}>{reviews.length}</Td>
+		<tr
+			style={clickableStyle}
+			onClick={handleClick}
+		>
+			{[code, title, avgRating, faculty, professor, reviews.length].map(
+				(content, i) => (
+					<Td
+						key={i}
+						to={`/courses/${course.id}`}
+					>
+						{content}
+					</Td>
+				)
+			)}
 		</tr>
 	);
 };
--- root/ui/src/components/ProfessorInfo.jsx
+++ root/ui/src/components/ProfessorInfo.jsx
@@ -2,29 +2,35 @@ import { useNavigate } from 'react-router-dom';
 import Td from './Td'; // helper that wraps content into a Link if a link is provided
 
 const ProfessorInfo = ({ professor }) => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 	const { name, avgRating, faculty, reviews, coursesTaught } = professor;
 
-  const clickableStyle = {
-    cursor: 'pointer',
-  }
+	const clickableStyle = {
+		cursor: 'pointer',
+	};
 
 	const courses = !coursesTaught
 		? 'Nothing added yet..'
 		: coursesTaught.map(course => course.code).join(', ');
 
-  const handleClick = () => {
-    navigate(`/professors/${professor.id}`);
-  }
+	const handleClick = () => {
+		navigate(`/professors/${professor.id}`);
+	};
 
 	return (
-			<tr style={clickableStyle} onClick={handleClick}>
-				<Td to={`/professors/${professor.id}`}>{name}</Td>
-				<Td to={`/professors/${professor.id}`}>{avgRating}</Td>
-				<Td to={`/professors/${professor.id}`}>{faculty}</Td>
-				<Td to={`/professors/${professor.id}`}>{reviews.length}</Td>
-				<Td to={`/professors/${professor.id}`}>{courses}</Td>
-			</tr>
+		<tr
+			style={clickableStyle}
+			onClick={handleClick}
+		>
+			{[name, avgRating, faculty, reviews.length, courses].map((content, i) => (
+				<Td
+					key={i}
+					to={`/professors/${professor.id}`}
+				>
+					{content}
+				</Td>
+			))}
+		</tr>
 	);
 };
 
